a7d49b74e3d5082a636fd5f21116bf7bddc7c037
Andrade-B-Bruna <62024853+Andrade-B-Bruna@users.noreply.github.com>
2020-04-30 18:38:37 -0300

    Update funcoes.js

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -8,8 +8,24 @@ function calcular(){
 var cat_op = parseInt(document.getElementById('cat_op').value);
 var cat_adj = parseInt(document.getElementById('cat_adj').value);
 
+let jsonData = {
+  "cat_op": document.getElementById('cat_op').value,
+  "cat_adj": document.getElementById('cat_adj').value
+}
+
+console.log(jsonData)
+
+axios.post('https://atlas-231814.appspot.com/calcula', jsonData)
+.then(response => {
+  console.log(response)
+})
+.catch(error => {
+  console.log(error)
+})
+
+/*
 hipotenusa = Math.hypot(cat_op, cat_adj);
 
-document.getElementById('hipotenusa').value = hipotenusa;
+document.getElementById('hipotenusa').value = hipotenusa;*/
 
 };
