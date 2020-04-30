96c7f6115d0b490c497fbf18945a40fa388030ee
Andrade-B-Bruna <62024853+Andrade-B-Bruna@users.noreply.github.com>
2020-04-30 15:26:04 -0300

    required

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -8,18 +8,8 @@ function calcular(){
 var cat_op = parseInt(document.getElementById('cat_op').value);
 var cat_adj = parseInt(document.getElementById('cat_adj').value);
 
-
-if((document.getElementById("cat_op").value=="") || (document.getElementById("cat_adj")).value == ""){
-
-    alert('Por favor, preencha os campos corretamente.');
-    document.getElementById("cat_op").focus();
-    return false
-
-}else{
-
 hipotenusa = Math.hypot(cat_op, cat_adj);
 
 document.getElementById('hipotenusa').value = hipotenusa;
 
-}
 };
